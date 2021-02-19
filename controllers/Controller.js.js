51cfe23a8484083798c88a58de1a71c9f2123a56
Haley Halsey <haleyhalsey@yahoo.com>
2021-02-18 20:56:15 -0800

    js

---
+++ controllers/Controller.js.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const router = express.Router();
+
+const burger = require("../models/burger.js");
+
+
+router.get("/", function (req, res) {
+  burger.all(function (data) {
+    const hbsObject = {
+      burgers: data
+    };
+    res.render("index", hbsObject);
+  });
+});
+
+router.post("/api/burgers", function (req, res) {
+  burger.create(req.body, function (result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  let condition;
+
+  burger.update(condition,req.params.id, function (result) {
+    if (result.changedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+router.delete("/api/burgers/:id", function (req, res) {
+  const condition = "id = " + req.params.id;
+
+  burger.delete(condition, function (result) {
+    if (result.affectedRows == 0) {
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+module.exports = router;
