239e304379acbc5bcf8072342f8da1fb23bbc0a2
Haley Halsey <haleyhalsey@yahoo.com>
2021-02-18 20:55:25 -0800

    mysql created

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+USE burgers_db;
+
+INSERT INTO burgers (name, devoured) VALUES('Good Burger', false);
+INSERT INTO burgers (name, devoured) VALUES('Medium Burger', false);
+INSERT INTO burgers (name, devoured) VALUES('BEST Burger', false);
