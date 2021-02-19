ddc8b1609fef255b67c669451af87073e10ba54b
Haley Halsey <haleyhalsey@yahoo.com>
2021-02-18 22:04:37 -0800

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,8 @@
 require("dotenv").config();
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_MARIA_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_MARIA_URL);
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
   var connection = mysql.createConnection({
     host: "localhost",
